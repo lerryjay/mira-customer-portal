9435959596429bfa3d4772873a5f38b32c2d12b5
Dominic Olajire E <lerryjay45@gmail.com>
2020-08-17 00:34:10 +0100

    completed product model

--- api/model/alerts.php
+++ api/model/alerts.php
@@ -1,8 +1,8 @@
 <?php 
   /**
-   * undocumented class
+   * Class for sending notifications and alerts
    */
-  class ClassName extends AnotherClass
+  class Alerts
   {
     /**
      * send email to client 
@@ -10,9 +10,42 @@
      * @param String $message - html string message
      * @return bool 
      **/
-    public function sendMail($email,$message)
+    public function sendMail($email,$subject,$message)
     {
+      require './phpmailer/class.phpmailer.php';
+      $headers = 'MIME-Version: 1.0'. "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859' . "\r\n";
+      $headers .= "From: notice@habopay.com";
+    
+      $mail = new PHPMailer;
+      $mail->isSMTP();
+      $mail->Host = 'mail.mirahpro.com';
+      $mail->SMTPSecure = 'ssl';
+      $mail->SMTPAuth = true;
+      $mail->SMTPDebug = false;
+      $mail->ContentType = 'text/html';
+      $mail->WordWrap = 300;
+      $mail->SMTPOptions = array(
+        'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+        )
+      );
+      //$mail->Host = 'smtp.gmail.com';
       
+      //$mail->SMTPSecure = "ssl";                          
+      //Set TCP port to connect to
+      $mail->Port = 465;        
+      $mail->Username = 'habopay@habopayapp.mirahpro.com';
+      $mail->Password = 'PasSw0rd75!@#';
+      $mail->setFrom('habopay@habopayapp.mirahpro.com');
+      $mail->addAddress($email);
+      $mail->Subject = $subject;
+      $mail->Body = $message;
+
+      if($mail->send()) echo json_encode(['result'=>'success','message'=>'Message sent!']);
+      else echo json_encode(['result'=>'error','message'=>'Sending failed!'. $mail->ErrorInfo]);
     }
 
     /**

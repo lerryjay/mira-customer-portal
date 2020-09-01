85c20dc1e0308e1d6229d817a6ad51725eb77faf
hameedhub <hameedabdulrahamann@gmail.com>
2020-09-01 16:23:12 +0100

    view client

--- src/common/global_constant.js
+++ src/common/global_constant.js
@@ -1,3 +1,4 @@
 
 export const HTTPURL = "http://192.168.1.4:8080/ticketapp/api/";
 export const FILEURL = "http://192.168.1.4:8080/ticketapp/public/";
+export const APIKEY = "97899c-7d0420-1273f0-901d29-84e2f8";

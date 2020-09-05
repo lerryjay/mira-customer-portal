6a8da1f178f9da3f9053a6b03ef3f823426ac887
Dammyton <owolabidamilola98@gmail.com>
2020-09-05 08:18:03 +0100

    added the files

--- src/App.js
+++ src/App.js
@@ -52,12 +52,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: sessionStorage.getItem("loggedIn"),
-      users: [],
-      profile: [],
+      loggedIn: true,
       admin: true,
-      getTicket: "",
-      getProduct: "",
     };
   }
 
@@ -138,10 +134,6 @@ class App extends Component {
       });
   };
 
-  getProfileDetails = () => {
-    console.log(this.state.profile, "Profile Info");
-  };
-
   changePassword = (data) => {
     const headers = new Headers();
     headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
@@ -158,142 +150,6 @@ class App extends Component {
       });
   };
 
-  createClient = (data) => {
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-    let form = new FormData(data);
-    return fetch(HTTPURL + "company/add", {
-      method: "POST",
-      body: form,
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
-  };
-
-  createTicket = (data) => {
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-    let form = new FormData(data);
-    return fetch(HTTPURL + "ticket/add", {
-      method: "POST",
-      body: form,
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
-  };
-
-  createProduct = (data) => {
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-    let form = new FormData(data);
-    return fetch(HTTPURL + "product/add", {
-      method: "POST",
-      body: form,
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
-  };
-
-  addPackage = (data) => {
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-    let form = new FormData(data);
-    return fetch(HTTPURL + "product/addmodule", {
-      method: "POST",
-      body: form,
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
-  };
-
-  sendChat = (data) => {
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-    let form = new FormData(data);
-
-    // form.append("file", file.get('file'));
-    // console.log(files[0]);
-
-    return fetch(HTTPURL + "ticket/replyticket", {
-      method: "POST",
-      body: form,
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
-  };
-
-  handleRoute = (e) => {
-    // Get Ticket by ID
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-
-    let ticketid = e.target.attributes.value.value;
-    // let ticketid = "5f4509c0c26d1"
-    let userid = "5f3e930330e28";
-
-    fetch(HTTPURL + `ticket/getticket?userid=${userid}&ticketid=${ticketid}`, {
-      method: "GET",
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.data, "data");
-        this.state.getTicket = data.data;
-        this.setState({ getTicket: data.data });
-
-        //  this.state.getTickets.push(data.data)
-        //  this.setState({getTicket: this.state.getTickets[0] })
-        //  console.log(this.state.getTicket, "data2")
-      });
-  };
-
-  handleProductRoute = (e) => {
-    // Get Ticket by ID
-    const headers = new Headers();
-    headers.append("API-KEY", "97899c-7d0420-1273f0-901d29-84e2f8");
-
-    let productid = e.target.attributes.value.value;
-    let userid = "5f44c8e94593e";
-
-    fetch(
-      HTTPURL + `product/getproduct?productid=${productid}&userid=${userid}`,
-      {
-        method: "GET",
-        headers: headers,
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.data, "data");
-        this.state.getProduct = data.data;
-        this.setState({ getProduct: data.data });
-
-        //  this.state.getTickets.push(data.data)
-        //  this.setState({getTicket: this.state.getTickets[0] })
-        //  console.log(this.state.getTicket, "data2")
-      });
-  };
-
   logoutUser = () =>
     this.setState({ loggedIn: sessionStorage.setItem("loggedIn", false) });
 
@@ -305,14 +161,6 @@ class App extends Component {
       signup: this.signupUser,
       forgotpassword: this.forgotPassword,
       changepassword: this.changePassword,
-      createclient: this.createClient,
-      createticket: this.createTicket,
-      createproduct: this.createProduct,
-      profiledetails: this.getProfileDetails,
-      getchat: this.sendChat,
-      viewmore: this.handleRoute,
-      viewmoredetails: this.handleProductRoute,
-      addpackage: this.addPackage,
     };
   };
 

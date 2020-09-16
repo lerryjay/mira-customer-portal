8a1f05619ad646e48d6d8517e11224c7636f1f54
Dominic Olajire E <lerryjay45@gmail.com>
2020-09-16 17:37:30 +0100

    fixed search clients page

--- src/common/global_constant.js
+++ src/common/global_constant.js
@@ -1,8 +1,8 @@
 
-export const HTTPURL =  "http://192.168.1.4:8080/ticketapp/api/"; 
-export const FILEURL = "http://192.168.1.4:8080/ticketapp/"; 
-// export const HTTPURL =  'http://localhost/ticketapp/api/';
-// export const FILEURL = 'http://localhost/ticketapp/';
+// export const HTTPURL =  "http://192.168.1.4:8080/ticketapp/api/"; 
+// export const FILEURL = "http://192.168.1.4:8080/ticketapp/"; 
+export const HTTPURL =  'http://localhost/ticketapp/api/';
+export const FILEURL = 'http://localhost/ticketapp/';
 export const APIKEY = "97899c-7d0420-1273f0-901d29-84e2f8";
 
 
